6530e6a045bb3d62671cd7b34f36abeb79c3f61e
Lazizbek0001 <normatovlaziz773@gmail.com>
2025-03-15 10:12:26 +0500

    new chat-app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'server',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +112,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "account.Account"
