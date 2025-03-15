2822a9504e7b12665a2a795247862a62aa7392c6
Lazizbek0001 <normatovlaziz773@gmail.com>
2025-03-15 15:51:24 +0500

    django spectacular

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'server',
     'account',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
