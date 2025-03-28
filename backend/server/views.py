19d2a3ad80afcb7db04e59c3d9ad5586dd86e754
Lazizbek <normatovlaziz9@gmail.com>
2025-03-28 09:46:32 +0500

    models added

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets  # Fixed typo
+from .models import Server
 
-# Create your views here.
+class ServerListView(viewsets.ViewSet):
+    queryset = Server.objects.all()  # Correct indentation
+
+    def list(self, request):  # Correct indentation
+        category = request.query_params.get("category")
+
+        if category:
+            self.queryset = self.queryset.filter(category=category)  # Fixed indentation
+
