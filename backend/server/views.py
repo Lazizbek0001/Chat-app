898cb6760876a2f1051b0ea0eaebde30c526fe4c
Lazizbek <normatovlaziz9@gmail.com>
2025-03-28 10:38:07 +0500

    Filtering by user

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets  # Fixed typo
+from rest_framework import viewsets 
 from .models import Server
 from .serializer import ServerSerializer
 from rest_framework.response import Response
 
 class ServerListView(viewsets.ViewSet):
-    queryset = Server.objects.all()  # Correct indentation
+    queryset = Server.objects.all() 
 
     def list(self, request):  # Correct indentation
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
-
+        by_user = request.query_params.get("by_user") == "true"
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
         if category:
             self.queryset = self.queryset.filter(category__name=category)
         if qty:
             self.queryset = self.queryset[: int(qty)]
-
+        
+            
         serializer = ServerSerializer(self.queryset, many=True)
 
         return Response(serializer.data)
