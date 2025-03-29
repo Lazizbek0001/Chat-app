a1e66932972618b3e9711a5cb8c9eac25439e5d6
Lazizbek <normatovlaziz773@gmail.com>
2025-03-29 15:40:48 +0500

    Models changed for image

--- backend/server/models.py
+++ backend/server/models.py
@@ -3,6 +3,11 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icons/{filename}"
+
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
 
 
 def category_icon_upload_path(instance, filename):
@@ -50,10 +55,31 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
+    banner = models.ImageField(upload_to=server_banner_upload_path,null=True,blank=True)
+    icon = models.ImageField(upload_to=server_icon_upload_path,null=True,blank=True)
+
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
         super(Channel, self).save(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+                super(Category, self).save(*args, **kwargs)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def Channel_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
+
 
 
     def __str__(self):
