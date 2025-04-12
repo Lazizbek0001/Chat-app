393392d979939b0cc183dbf3b0a279196e1d58de
Lazizbek <normatovlaziz773@gmail.com>
2025-04-12 13:44:32 +0500

    AppBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import { ThemeProvider } from "@emotion/react";
 
-function App() {
-
-
-  return (
-    <>
-     
-    </>
+import createMuiTheme from "./theme/theme";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
   )
-}
+)
 
-export default App
+const App = () => {
+  const theme = createMuiTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+    );
+};
+export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Box, CssBaseline } from "@mui/material";
+import PrimaryAppBar from "./templates/PrimaryAppBar";
+
+
+const Home = () => {
+    
+    return (<Box sx={{display:"flex"}}>
+        <CssBaseline /> 
+        <PrimaryAppBar />
+        </Box>
+        );
+};
+
+export default Home;
